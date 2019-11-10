4c8b6cd44eaa282bfc0309af16f4413c907c956e
César Gutiérrez Olivares <cesar.olivares@hey.car>
2019-11-10 23:09:47 +0100

    chore: add google analytics

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -69,7 +69,22 @@ export const SEO = ({ description, lang, meta, title }) => {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-144514372-1"
+      ></script>
+      <script>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-144514372-1');
+      `}
+      </script>
+    </Helmet>
   )
 }
 
